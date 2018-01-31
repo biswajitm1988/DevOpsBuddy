7dcbc79946a7e8cce73ade4ae3e4af01c0e83450
Biswajit Mohapatra <biswajit1988@gmail.com>
2018-01-30 20:03:38 -0500

    password encryption

--- src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -10,6 +10,7 @@ import com.devopsbuddy.enums.PlansEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,9 +33,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     @Transactional
     public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles){
+
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
         Plan plan = new Plan(plansEnum);
         // It makes sure the plans exist in the database
         if (!planRepository.exists(plansEnum.getId())) {
